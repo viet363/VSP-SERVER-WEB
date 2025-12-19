604a70cf6790e6f134111c4626bfc48bbcc124fa
LQV <lequocviet362003@gmail.com>
2025-12-19 15:00:59 +0700

    fix conection railway

--- db.js
+++ db.js
@@ -1,21 +1,27 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "localhost",
+  host: "trolley.proxy.rlwy.net",
+  port: 24743, 
   user: "root",
-  password: "lequocviet362003@",
+  password: "oPLililtxmoIoRFNglgILWBpWHFakVAT",
   database: "vsp",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const testConnection = async () => {
   try {
     const conn = await db.getConnection();
-    console.log("Kết nối MySQL thành công!");
+
+    console.log("Kết nối MySQL Railway thành công:", rows);
+
     conn.release();
   } catch (err) {
-    console.error("Kết nối thất bại:", err);
+    console.error("Kết nối thất bại:", err.message);
   }
 };
